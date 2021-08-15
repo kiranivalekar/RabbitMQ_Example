bddf2e8a1d853df58b4b044cd66f65095cfbb0ca
Kiran Ivalekar <38786126+kiranivalekar@users.noreply.github.com>
2021-08-15 17:04:10 +0530

    Initial Commit RabbitMQ Example

---
+++ main/app/Providers/EventServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+
+class EventServiceProvider extends ServiceProvider
+{
+    
+    public function boot()
+    {
+        \App::bindMethod(TestJob::class . '@handle', function($job){
+            return $job->handle();
+        });
+    }
+}
